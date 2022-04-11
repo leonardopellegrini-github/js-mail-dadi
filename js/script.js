156c6f5a5c61500951dbe76ad2515fad89f0e215
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-11 16:17:17 +0200

    inserisco struttura html css e scrivo passaggi del file js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*
+
+CONTROLLARE LE EMAIL
+
+1- inserire un database di email
+2- collegare l'input in modo da controllare il valore inserito
+3- creare if per vedere se l'email inserita fa parte
+
+*/
+
+//inserisco database
+const database = ['leonardo@gmail.com', 'franco@hotmail.com', 'bamba@hotmail.com','capo@capo.au', 'prova@prova.org', 'noname@hotmail.com', 'yolo@mail.it', 'lpg@account.com',]
+
+
+
+
+
+
+/*
+
+LANCIA I DADI
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1- alla pressione del pulsante viene generato un numero compreso da 1 a 6 sia per il giocatore che per il computer
+2- creare if per vedere se un numero è maggiore dell'altro.
+3- stampare i valori e mostrare il testo con "HAI VINTO" o "HAI PERSO"
+
+*/
+
